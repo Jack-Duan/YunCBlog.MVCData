582e8b70cc0eeb3d1169334b9f45efa8c289c05a
段晓阳 <1025274058@qq.com>
2019-11-06 17:37:39 +0800

    添加项目文件。

---
+++ YunCBlog.MVCData/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace YunCBlog.MVCData
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
