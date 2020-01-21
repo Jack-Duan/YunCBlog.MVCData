36a5224f13a0776e7d2785de740e17671c3e6a4d
段晓阳 <1025274058@qq.com>
2020-01-21 16:17:45 +0800

    排除本地记录

--- YunCBlog.MVCData/Filters/AccessLogAttribute.cs
+++ YunCBlog.MVCData/Filters/AccessLogAttribute.cs
@@ -16,22 +16,27 @@ namespace YunCBlog.MVCData.Filters
         {
             //filterContext.HttpContext.Request
 
-            IBLL.IAccessListVistor accessManager = new BLL.AccessListVistor();
-            var result = await accessManager.CreateModel(new Dto.AccessListDto
+            var ip = filterContext.HttpContext.Request.ServerVariables["Remote_Host"];
+            if (ip != "218.24.105.82" && !filterContext.HttpContext.Request.IsLocal)
             {
-                Browser = filterContext.HttpContext.Request.ServerVariables["Browser"],
-                Http_Referer = filterContext.HttpContext.Request.ServerVariables["Http_Referer"],
-                IP = filterContext.HttpContext.Request.ServerVariables["Server_Name"],
-                Local_Addr = filterContext.HttpContext.Request.ServerVariables["Local_Addr"],
-                PageName = filterContext.HttpContext.Request.Path,
-                MajorVersion = filterContext.HttpContext.Request.ServerVariables["MajorVersion"],
-                RealmName = filterContext.HttpContext.Request.ServerVariables["RealmName"],
-                Remote_Addr = filterContext.HttpContext.Request.ServerVariables["Remote_Addr"],
-                Remote_Host = filterContext.HttpContext.Request.ServerVariables["Remote_Host"],
-                Platform = filterContext.HttpContext.Request.ServerVariables["Platform"],
-                Server_Name = filterContext.HttpContext.Request.ServerVariables["Server_Name"],
-                Server_Port = filterContext.HttpContext.Request.ServerVariables["Server_Port"]
-            }).ConfigureAwait(false);
+
+                IBLL.IAccessListVistor accessManager = new BLL.AccessListVistor();
+                var result = await accessManager.CreateModel(new Dto.AccessListDto
+                {
+                    Browser = filterContext.HttpContext.Request.ServerVariables["Browser"],
+                    Http_Referer = filterContext.HttpContext.Request.ServerVariables["Http_Referer"],
+                    IP = filterContext.HttpContext.Request.ServerVariables["Remote_Host"],
+                    Local_Addr = filterContext.HttpContext.Request.ServerVariables["Local_Addr"],
+                    PageName = filterContext.HttpContext.Request.Path,
+                    MajorVersion = filterContext.HttpContext.Request.ServerVariables["MajorVersion"],
+                    RealmName = filterContext.HttpContext.Request.ServerVariables["RealmName"],
+                    Remote_Addr = filterContext.HttpContext.Request.ServerVariables["Remote_Addr"],
+                    Remote_Host = filterContext.HttpContext.Request.ServerVariables["Remote_Host"],
+                    Platform = filterContext.HttpContext.Request.ServerVariables["Platform"],
+                    Server_Name = filterContext.HttpContext.Request.ServerVariables["Server_Name"],
+                    Server_Port = filterContext.HttpContext.Request.ServerVariables["Server_Port"]
+                }).ConfigureAwait(false);
+            }
         }
     }
 }
